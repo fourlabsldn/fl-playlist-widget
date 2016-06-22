4ccd19491f5fd66d8b30554d4d6c6f330b6b1b98
lazamar <lazaronijunior@gmail.com>
2016-06-22 19:08:01 +0100

    Add more songs

--- src/js/TrackList.js
+++ src/js/TrackList.js
@@ -59,7 +59,117 @@ export default class TrackList extends ViewController {
         "track_number" : 5,
         "type" : "track",
         "uri" : "spotify:track:4i3txPQIUV4eC9g9FBpi9I"
-      }
+      },
+      {
+  "album" : {
+    "album_type" : "album",
+    "available_markets" : [ "AR", "AU", "AT", "BE", "BO", "BR", "BG", "CA", "CL", "CO", "CR", "CY", "CZ", "DK", "DO", "DE", "EC", "EE", "SV", "FI", "FR", "GR", "GT", "HN", "HK", "HU", "IS", "IE", "IT", "LV", "LT", "LU", "MY", "MX", "NL", "NZ", "NI", "NO", "PA", "PY", "PE", "PH", "PL", "PT", "SG", "SK", "ES", "SE", "CH", "TW", "TR", "UY", "US", "GB", "AD", "LI", "MC", "ID" ],
+    "external_urls" : {
+      "spotify" : "https://open.spotify.com/album/0UccZZgelTAbbk3OSPZymO"
+    },
+    "href" : "https://api.spotify.com/v1/albums/0UccZZgelTAbbk3OSPZymO",
+    "id" : "0UccZZgelTAbbk3OSPZymO",
+    "images" : [ {
+      "height" : 640,
+      "url" : "https://i.scdn.co/image/f66195f98d32ffb0f1fcca0ea9e69e2794ec6742",
+      "width" : 640
+    }, {
+      "height" : 300,
+      "url" : "https://i.scdn.co/image/1f594d484a753cf21d909f3eaf0c3953d7caca61",
+      "width" : 300
+    }, {
+      "height" : 64,
+      "url" : "https://i.scdn.co/image/f323863361593570fe9a932e006a5a8b834991ec",
+      "width" : 64
+    } ],
+    "name" : "Greatest Hits Volume One - The Singles",
+    "type" : "album",
+    "uri" : "spotify:album:0UccZZgelTAbbk3OSPZymO"
+  },
+  "artists" : [ {
+    "external_urls" : {
+      "spotify" : "https://open.spotify.com/artist/2sil8z5kiy4r76CRTXxBCA"
+    },
+    "href" : "https://api.spotify.com/v1/artists/2sil8z5kiy4r76CRTXxBCA",
+    "id" : "2sil8z5kiy4r76CRTXxBCA",
+    "name" : "The Goo Goo Dolls",
+    "type" : "artist",
+    "uri" : "spotify:artist:2sil8z5kiy4r76CRTXxBCA"
+  } ],
+  "available_markets" : [ "AR", "AU", "AT", "BE", "BO", "BR", "BG", "CA", "CL", "CO", "CR", "CY", "CZ", "DK", "DO", "DE", "EC", "EE", "SV", "FI", "FR", "GR", "GT", "HN", "HK", "HU", "IS", "IE", "IT", "LV", "LT", "LU", "MY", "MX", "NL", "NZ", "NI", "NO", "PA", "PY", "PE", "PH", "PL", "PT", "SG", "SK", "ES", "SE", "CH", "TW", "TR", "UY", "US", "GB", "AD", "LI", "MC", "ID" ],
+  "disc_number" : 1,
+  "duration_ms" : 238333,
+  "explicit" : false,
+  "external_ids" : {
+    "isrc" : "USWB10704696"
+  },
+  "external_urls" : {
+    "spotify" : "https://open.spotify.com/track/7p1PhtGLjq0ISncRXBHqXY"
+  },
+  "href" : "https://api.spotify.com/v1/tracks/7p1PhtGLjq0ISncRXBHqXY",
+  "id" : "7p1PhtGLjq0ISncRXBHqXY",
+  "name" : "Here Is Gone",
+  "popularity" : 52,
+  "preview_url" : "https://p.scdn.co/mp3-preview/4a8b9f71672407eeae6b138cf27ad1613cafe767",
+  "track_number" : 3,
+  "type" : "track",
+  "uri" : "spotify:track:7p1PhtGLjq0ISncRXBHqXY"
+},
+{
+  "album" : {
+    "album_type" : "album",
+    "available_markets" : [ "AR", "AU", "AT", "BE", "BO", "BR", "BG", "CA", "CL", "CO", "CR", "CY", "CZ", "DK", "DO", "DE", "EC", "EE", "SV", "FI", "FR", "GR", "GT", "HN", "HK", "HU", "IS", "IE", "IT", "LV", "LT", "LU", "MY", "MT", "MX", "NL", "NZ", "NI", "NO", "PA", "PY", "PE", "PH", "PL", "PT", "SG", "SK", "ES", "SE", "CH", "TW", "TR", "UY", "US", "GB", "AD", "LI", "MC", "ID" ],
+    "external_urls" : {
+      "spotify" : "https://open.spotify.com/album/67cksuMf5EvK5pu1DwGeFi"
+    },
+    "href" : "https://api.spotify.com/v1/albums/67cksuMf5EvK5pu1DwGeFi",
+    "id" : "67cksuMf5EvK5pu1DwGeFi",
+    "images" : [ {
+      "height" : 640,
+      "url" : "https://i.scdn.co/image/cc5549f65c77d85b5b5405ba3a18f15995a7be9b",
+      "width" : 640
+    }, {
+      "height" : 300,
+      "url" : "https://i.scdn.co/image/265b55a307f70ba0c3f4770843601ab7130f0de6",
+      "width" : 300
+    }, {
+      "height" : 64,
+      "url" : "https://i.scdn.co/image/82363a9ee7c45f8afce1df0e541e56dea1be2a33",
+      "width" : 64
+    } ],
+    "name" : "Illusions",
+    "type" : "album",
+    "uri" : "spotify:album:67cksuMf5EvK5pu1DwGeFi"
+  },
+  "artists" : [ {
+    "external_urls" : {
+      "spotify" : "https://open.spotify.com/artist/0NSO0g40h9CTj13hKPskeb"
+    },
+    "href" : "https://api.spotify.com/v1/artists/0NSO0g40h9CTj13hKPskeb",
+    "id" : "0NSO0g40h9CTj13hKPskeb",
+    "name" : "Ibrahim Maalouf",
+    "type" : "artist",
+    "uri" : "spotify:artist:0NSO0g40h9CTj13hKPskeb"
+  } ],
+  "available_markets" : [ "AR", "AU", "AT", "BE", "BO", "BR", "BG", "CA", "CL", "CO", "CR", "CY", "CZ", "DK", "DO", "DE", "EC", "EE", "SV", "FI", "FR", "GR", "GT", "HN", "HK", "HU", "IS", "IE", "IT", "LV", "LT", "LU", "MY", "MT", "MX", "NL", "NZ", "NI", "NO", "PA", "PY", "PE", "PH", "PL", "PT", "SG", "SK", "ES", "SE", "CH", "TW", "TR", "UY", "US", "GB", "AD", "LI", "MC", "ID" ],
+  "disc_number" : 1,
+  "duration_ms" : 291303,
+  "explicit" : false,
+  "external_ids" : {
+    "isrc" : "FRP8H1300280"
+  },
+  "external_urls" : {
+    "spotify" : "https://open.spotify.com/track/5EzGOkUwkRUXYAyvjlEHah"
+  },
+  "href" : "https://api.spotify.com/v1/tracks/5EzGOkUwkRUXYAyvjlEHah",
+  "id" : "5EzGOkUwkRUXYAyvjlEHah",
+  "name" : "True Sorry",
+  "popularity" : 49,
+  "preview_url" : "https://p.scdn.co/mp3-preview/6b6beff68189d762fb03f3a24c5ada56c6232f61",
+  "track_number" : 8,
+  "type" : "track",
+  "uri" : "spotify:track:5EzGOkUwkRUXYAyvjlEHah"
+}
     ];
 
     this.setTracks(this.tracks);
