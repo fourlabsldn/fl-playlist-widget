9c5f8dd4c9258a3cecd15b71032dfd31d6d20c34
Jonny Schmid <hi@jonathanschmid.de>
2016-06-29 12:28:29 +0100

    Prettify UI

--- src/js/SearchBox.js
+++ src/js/SearchBox.js
@@ -12,18 +12,23 @@ export default class SearchBox extends ViewController {
   buildHtml() {
     super.buildHtml();
 
-    const icon = document.createElement('div');
-    this.html.icon = icon;
-    icon.innerHTML = constants.soundNoteIcon;
-    icon.classList.add(`${this.cssPrefix}-icon`, 'btn', 'btn-default');
-    this.html.container.appendChild(icon);
+    const range = document.createRange();
+    range.selectNode(document.body);
 
-    const textInput = document.createElement('input');
-    this.html.textInput = textInput;
-    textInput.setAttribute('type', 'text');
-    textInput.classList.add(`${this.cssPrefix}-textInput`, 'form-control');
-    this.html.container.appendChild(textInput);
-    textInput.addEventListener('keydown', (e) => {
+    const searchBoxFragment = range.createContextualFragment(`
+      <div class="input-group">
+          <span class="input-group-addon">
+            <svg width="14" height="14" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg" style="opacity:.4">
+              <path d="M1216 832q0-185-131.5-316.5t-316.5-131.5-316.5 131.5-131.5 316.5 131.5 316.5 316.5 131.5 316.5-131.5 131.5-316.5zm512 832q0 52-38 90t-90 38q-54 0-90-38l-343-342q-179 124-399 124-143 0-273.5-55.5t-225-150-150-225-55.5-273.5 55.5-273.5 150-225 225-150 273.5-55.5 273.5 55.5 225 150 150 225 55.5 273.5q0 220-124 399l343 343q37 37 37 90z"/>
+            </svg>
+          </span>
+          <input type="text" placeholder="Search Spotify..." class="form-control ${this.cssPrefix}-textInput">
+      </div>
+    `);
+
+    this.html.textInput = searchBoxFragment.querySelector('input');
+
+    this.html.textInput.addEventListener('keydown', (e) => {
       const enterKeyCode = 13;
       const keyPressedCode = e.keyCode ? e.keyCode : e.which;
       if (keyPressedCode === enterKeyCode) {
@@ -32,6 +37,8 @@ export default class SearchBox extends ViewController {
         this.trigger('usertyping', keyPressedCode);
       }
     });
+
+    this.html.container.appendChild(searchBoxFragment);
   }
 
   /**
